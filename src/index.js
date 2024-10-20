b825ad0fc653729516e71f6e84f62bbdbb11871a
Om Bhor <ombhor617@gmail.com>
2024-10-20 21:55:33 +0530

    mongodb connected succesfully

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import dotenv from 'dotenv';
 
 import mongoose, { connect } from 'mongoose';
-import { DB_NAME } from './constants';
+import { DB_NAME } from './constants.js';
 import connectDB from './db/index.js';
 dotenv.config({
     path: './.env'
